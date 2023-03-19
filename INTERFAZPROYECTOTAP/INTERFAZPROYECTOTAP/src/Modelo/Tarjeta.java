4237176c6ae91575557983f3d0b91c3d6fd7ae72
Darth_Yisus2099 <65199911+yisus2099@users.noreply.github.com>
2023-03-18 20:38:20 -0600

    Add files via upload

---
+++ INTERFAZPROYECTOTAP/INTERFAZPROYECTOTAP/src/Modelo/Tarjeta.java
@@ -0,0 +1,122 @@
+package Modelo;
+
+import Modelo.Movimiento;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Tarjeta {
+    private  String numCuenta, numTarjeta;
+    private Double limCredito, tasaInteresAnual, anualidad;
+    private Date fechaCreacion, fechaCorte,  fechaPago, fechaApliAnua;
+    ArrayList <Movimiento> repositorioDeMovimientos;
+    
+    public Tarjeta() {
+        repositorioDeMovimientos = new ArrayList<Movimiento>();
+    }
+
+//Lista de movimientos
+    public ArrayList<Movimiento> getRepositorioDeMovimientos() {
+        return repositorioDeMovimientos;
+    }
+
+    public void setRepositorioDeMovimientos(ArrayList<Movimiento> repositorioDeMovimientos) {
+        this.repositorioDeMovimientos = repositorioDeMovimientos;
+    }
+    
+    public void addMovimiento(Movimiento m){
+        repositorioDeMovimientos.add(m);
+    }
+    
+    //Atributos de la clase
+
+    public Tarjeta(String numCuenta, String numTarjeta, Double limCredito, Double tasaInteresAnual, Double anualidad, Date fechaCreacion, Date fechaCorte, Date fechaPago, Date fechaApliAnua) {
+        this.numCuenta = numCuenta;
+        this.numTarjeta = numTarjeta;
+        this.limCredito = limCredito;
+        this.tasaInteresAnual = tasaInteresAnual;
+        this.anualidad = anualidad;
+        this.fechaCreacion = fechaCreacion;
+        this.fechaCorte = fechaCorte;
+        this.fechaPago = fechaPago;
+        this.fechaApliAnua = fechaApliAnua;
+    }
+
+    public String getNumCuenta() {
+        return numCuenta;
+    }
+
+    public void setNumCuenta(String numCuenta) {
+        this.numCuenta = numCuenta;
+    }
+
+    public String getNumTarjeta() {
+        return numTarjeta;
+    }
+
+    public void setNumTarjeta(String numTarjeta) {
+        this.numTarjeta = numTarjeta;
+    }
+
+    public Double getLimCredito() {
+        return limCredito;
+    }
+
+    public void setLimCredito(Double limCredito) {
+        this.limCredito = limCredito;
+    }
+
+    public Double getTasaInteresAnual() {
+        return tasaInteresAnual;
+    }
+
+    public void setTasaInteresAnual(Double tasaInteresAnual) {
+        this.tasaInteresAnual = tasaInteresAnual;
+    }
+
+    public Double getAnualidad() {
+        return anualidad;
+    }
+
+    public void setAnualidad(Double anualidad) {
+        this.anualidad = anualidad;
+    }
+
+    public Date getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Date fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Date getFechaCorte() {
+        return fechaCorte;
+    }
+
+    public void setFechaCorte(Date fechaCorte) {
+        this.fechaCorte = fechaCorte;
+    }
+
+    public Date getFechaPago() {
+        return fechaPago;
+    }
+
+    public void setFechaPago(Date fechaPago) {
+        this.fechaPago = fechaPago;
+    }
+
+    public Date getFechaApliAnua() {
+        return fechaApliAnua;
+    }
+
+    public void setFechaApliAnua(Date fechaApliAnua) {
+        this.fechaApliAnua = fechaApliAnua;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarjeta{" + "numCuenta=" + numCuenta + ", numTarjeta=" + numTarjeta + ", limCredito=" + limCredito + ", tasaInteresAnual=" + tasaInteresAnual + ", anualidad=" + anualidad + ", fechaCreacion=" + fechaCreacion + ", fechaCorte=" + fechaCorte + ", fechaPago=" + fechaPago + ", fechaApliAnua=" + fechaApliAnua + '}';
+    }
+    
+    
+}
